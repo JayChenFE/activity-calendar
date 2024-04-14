768da76f2d9e922126b6cf5054e1a71945ebfd2c
jaychenfe <chenjiehwy@163.com>
2024-04-14 17:51:46 +0800

    refactor activityUtil.js

--- activityUtils.js
+++ activityUtils.js
@@ -1,3 +1,20 @@
+const activityHandlers = {
+  daily: getDailyActivity,
+  weekly: getWeeklyActivity,
+  interval: getIntervalActivity,
+}
+
+const dayOfWeek = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+]
+
+// 根据日期和规则获取活动列表
 function getActivitiesByDate(rules, date) {
   const activities = []
   const targetDayOfWeek = getTargetDayOfWeek(date)
@@ -12,12 +29,7 @@ function getActivitiesByDate(rules, date) {
   return activities
 }
 
-const activityHandlers = {
-  daily: getDailyActivity,
-  weekly: getWeeklyActivity,
-  interval: getIntervalActivity,
-}
-
+// 根据规则类型调用相应的处理程序
 function getActivityByRule(rule, targetDayOfWeek, date) {
   const { type } = rule
   const handler = activityHandlers[type]
@@ -27,6 +39,7 @@ function getActivityByRule(rule, targetDayOfWeek, date) {
   return null
 }
 
+// 获取每日活动
 function getDailyActivity({ rule }) {
   const { start_time, content } = rule
   return {
@@ -35,6 +48,7 @@ function getDailyActivity({ rule }) {
   }
 }
 
+// 获取每周活动
 function getWeeklyActivity({ rule, targetDayOfWeek }) {
   const { start_time, content, day_of_week } = rule
   if (day_of_week === targetDayOfWeek) {
@@ -46,6 +60,7 @@ function getWeeklyActivity({ rule, targetDayOfWeek }) {
   return null
 }
 
+// 获取间隔活动
 function getIntervalActivity({ rule, date }) {
   const { start_time, content, start_date, interval_days } = rule
   if (isDateIntervalMatch(start_date, date, interval_days)) {
@@ -55,29 +70,20 @@ function getIntervalActivity({ rule, date }) {
     }
   }
   return null
-
-  function isDateIntervalMatch(start_date, date, interval_days) {
-    const startDate = new Date(start_date)
-    const currentDate = new Date(date)
-    const daysDiff = Math.floor((currentDate - startDate) / (1000 * 3600 * 24))
-    return daysDiff >= 0 && daysDiff % interval_days === 0
-  }
 }
 
-function getTargetDayOfWeek(date) {
-  const dayOfWeek = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-  ]
+// 判断日期是否符合间隔要求
+function isDateIntervalMatch(start_date, date, interval_days) {
+  const startDate = new Date(start_date)
+  const currentDate = new Date(date)
+  const daysDiff = Math.floor((currentDate - startDate) / (1000 * 3600 * 24))
+  return daysDiff >= 0 && daysDiff % interval_days === 0
+}
 
+// 获取目标日期的星期几
+function getTargetDayOfWeek(date) {
   const targetDate = new Date(date)
   const targetDayOfWeek = dayOfWeek[targetDate.getDay()]
-
   return targetDayOfWeek
 }
 
